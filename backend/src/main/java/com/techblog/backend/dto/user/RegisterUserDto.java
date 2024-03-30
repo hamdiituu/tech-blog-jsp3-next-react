7e5e18450a6f44e136ae84045ae4959c1609f358
hamditug <hamditugmobil@gmail.com>
2024-03-30 15:58:00 +0300

    register user enpoint add

--- backend/src/main/java/com/techblog/backend/dto/user/RegisterUserDto.java
+++ backend/src/main/java/com/techblog/backend/dto/user/RegisterUserDto.java
@@ -1,15 +1,28 @@
 package com.techblog.backend.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-
+import lombok.Setter;
 @Data
-@NoArgsConstructor
+@Getter
+@Setter
 @AllArgsConstructor
-
+@NoArgsConstructor
 public class RegisterUserDto {
+    @NotEmpty
+    @Size(min = 3, message = "En az 3 karakter giriniz.")
     private String username;
+
+    @NotEmpty
+    @Email
     private String email;
+
+    @NotEmpty
+    @Size(min = 6, message = "En az 6 karakter giriniz.")
     private String password;
 }
