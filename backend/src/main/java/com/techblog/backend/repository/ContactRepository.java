1619825f078ac238e5fb1bc3fcff469dc6cbf376
hamditug <hamditugmobil@gmail.com>
2024-04-08 04:01:12 +0300

    repository refactor

--- backend/src/main/java/com/techblog/backend/repository/ContactRepository.java
+++ backend/src/main/java/com/techblog/backend/repository/ContactRepository.java
@@ -2,12 +2,13 @@ package com.techblog.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.techblog.backend.model.Contact;
 
 public interface ContactRepository extends JpaRepository<Contact, Long> {
-    @Query(nativeQuery = true, value = "SELECT * FROM contact WHERE name regexp ?1 OR email regexp ?1 OR subject regexp ?1 OR phone regexp ?1 OR message regexp ?1")
-    Page<Contact> findByRegex(String regex, Pageable pageable);
+    @Query(nativeQuery = true, value = "SELECT * FROM contact WHERE name regexp :searchTerm OR email regexp :searchTerm OR subject regexp :searchTerm OR phone regexp :searchTerm OR message regexp :searchTerm")
+    Page<Contact> findByRegex(@Param("searchTerm") String searchTerm, Pageable pageable);
 }
