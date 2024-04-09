433eaf04e041d6bdc810ade3d388dc751919f07c
hamditug <hamditugmobil@gmail.com>
2024-04-10 01:01:51 +0300

    category model add

---
+++ backend/src/main/java/com/techblog/backend/model/Category.java
@@ -0,0 +1,28 @@
+package com.techblog.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "category")
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private boolean isActive;
+ 
+}
